93860af6fe013a86d6ac8e5db3514e81ebd3f331
Mihir Shankar <mihir.shankar20@gmail.com>
2026-03-01 15:18:50 -0800

    added instant vercel deploy update

--- apps/mobile/src/pages/home/DashboardPage.tsx
+++ apps/mobile/src/pages/home/DashboardPage.tsx
@@ -112,7 +112,7 @@ export function DashboardPage() {
   });
   const [picklistSelectorOpen, setPicklistSelectorOpen] = useState(false);
 
-  const OUR_TEAM = 8724;
+  const OUR_TEAM = 846;
 
   // DEV TEST: Set to true to test future match UI with mock data
   const DEV_TEST_FUTURE = false;
